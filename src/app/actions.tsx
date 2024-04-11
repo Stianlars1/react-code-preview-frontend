3abd5aa2cdec415d3935143d5885fb0ee02863c1
stian.larsen <stian.larsen@sparebank1.no>
2024-04-11 15:13:02 +0200

    Added force re-fetch of data

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,7 +1,8 @@
 import { MonthlyDownloadsUrl } from "@/utils/constants";
 
+export const dynamic = "force-dynamic";
 export const totalDownloads = async () => {
-  const res = await fetch(MonthlyDownloadsUrl);
+  const res = await fetch(MonthlyDownloadsUrl, { next: { revalidate: 60 } });
   const data = await res.json();
   return data.downloads;
 };
